f1df67e2b5858b5c8d21b5142b7224b17aee9be9
Taloir <taloir.twl@gmail.com>
2025-07-23 17:55:09 -0600

    added timestamp

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,5 +1,7 @@
 const getString = window.location.search;
-
 const myInfo = new URLSearchParams(getString);
+
+const timestamp = myInfo.get('timestamp')
+timestamp.value = new Date()
 
 document.querySelector("#results").innerHTML = `<p>Applicant: ${myInfo.get('first')} ${myInfo.get('last')}, ${myInfo.get('title')} at ${myInfo.get('company')}</p><p>Membership Level: ${myInfo.get('tier')}</p><p>Phone: ${myInfo.get('phone')}</p><p>Email: ${myInfo.get('email')}</p>`
